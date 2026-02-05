b90dbb69a40077d4f78c356a057bb2c16708e6a5
Jacob Hague <HagueJ@cardiff.ac.uk>
2026-02-05 17:37:11 +0000

    added handle fuct via props

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,11 @@ function App() {
   ];
 
     
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
 
-
-  return <div><ListGroup items={items} heading='Cities' /></div>
+  return <div><ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem} /></div>
 }
 
 export default App;
